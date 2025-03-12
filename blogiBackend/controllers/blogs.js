42075dc6033316a1004781ba03b4df0ff8f48c74
Lauri Suhonen <suhari.suhonen@gmail.com>
2025-03-12 22:58:30 +0200

    adsasdgasuidgasgdasgd

--- blogiBackend/controllers/blogs.js
+++ blogiBackend/controllers/blogs.js
@@ -1,23 +1,62 @@
 const blogRoutes = require('express').Router()
 const Blog = require('../models/blog.js')
 
-blogRoutes.get('/', (req, res, next) => {
-    Blog.find({})
-        .then(blogs => {
-          res.json(blogs)
-        })
-        .catch(error => next(error))
+blogRoutes.get('/', async (req, res, next) => {
+
+    try {
+      const blogs = await Blog.find({})
+      res.json(blogs)
+    } catch (error) {
+      next(error)
+    }
 })
 
-blogRoutes.post('/', (req, res, next) => {
-    const blog = new Blog(req.body)
+blogRoutes.post('/', async (req, res, next) => {
 
-    blog
-      .save()
-      .then(result => {
-          res.status(201).json(result)
-      })
-      .catch(error => next(error))
+    const blog = new Blog({
+      title: req.body.title,
+      author: req.body.author,
+      url: req.body.url,
+      likes: req.body.likes || 0,
+    })
+
+    try {
+      const result = await blog.save()
+      res.status(201).json(result)
+    } catch (error) {
+      next(error)
+    }
+})
+
+blogRoutes.delete('/:id', async (req, res, next) => {
+
+  try {
+    await Blog.findByIdAndDelete(req.params.id)
+    res.status(204).end()
+  } catch (error) {
+    next(error)
+  }
+})
+
+blogRoutes.put('/:id', async (req, res, next) => {
+
+  try {
+    const changedLikes = {
+      title: req.body.title,
+      author: req.body.author,
+      url: req.body.url,
+      likes: req.body.likes,
+    }
+    console.log(changedLikes, 'Changed LIkes')
+
+    const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, changedLikes, {new: true})
+    console.log(updatedBlog, 'updated Blog')
+
+    res.json(updatedBlog)
+  } catch (error) {
+    next(error)
+  }
+
 })
 
 module.exports = blogRoutes
--- blogiBackend/tests/blog_api.test.js
+++ blogiBackend/tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
@@ -11,17 +11,107 @@ const api = supertest(app)
 
 const manyBlogs = blogs.listWithManyBlogs
 
-beforeEach(async () => {
-    await Blog.deleteMany({})
-    await Blog.insertMany(manyBlogs)
+describe('Normal GET:s works', () => {
+    beforeEach(async () => {
+        await Blog.deleteMany({})
+        await Blog.insertMany(manyBlogs)
+    })
+    
+    test('Right amount of JSON blogs', async () => {
+    
+        const response = await api.get('/api/blogs').expect('Content-Type', /application\/json/)
+    
+        assert.strictEqual(response.body.length, manyBlogs.length)
+    })
+
+    test('id is in right format', async () => {
+        const firstBlog = (await api.get('/api/blogs')).body[0]
+        const keys = Object.keys(firstBlog)
+        
+        assert.strictEqual(keys.includes('id'), true)
+    })
 })
 
-test('Right amount of JSON blogs', async () => {
+describe('POST works', () => {
 
-    const response = await api.get('/api/blogs').expect('Content-Type', /application\/json/)
+    test('HTTP POST works fine', async () => {
+        const newBlog = {
+            title: 'Go To Statement Considered Harmfullll',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+        }
+        
+        await api.post('/api/blogs').send(newBlog).expect(201)
+        const response = await api.get('/api/blogs')
+        const contents = response.body.map(r => r.title)
+        
+        assert.strictEqual(manyBlogs.length + 1, response.body.length)
+        assert(contents.includes('Go To Statement Considered Harmfullll'))
+    })
+    
+    test('Likes equal 0 if not given', async () => {
+        const newBlog = {
+            title: 'Go To Statement Considered Harmfullll',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        }
+    
+        await api.post('/api/blogs').send(newBlog).expect(201)
+        const response = await api.get('/api/blogs')
+        const objects = response.body
+        assert(objects.some(b => b.title === 'Go To Statement Considered Harmfullll' && b.likes === 0))
+    })
+    
+    test('return 400 if no title or url', async () => {
+    
+        const noTitle = {
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+        }
+    
+        const noUrl = {
+            title: 'Go To Statement Considered Harmfullll',
+            author: 'Edsger W. Dijkstra',
+            likes: 5,
+        }
+    
+        await api.post('/api/blogs').send(noTitle).expect(400)
+        await api.post('/api/blogs').send(noUrl).expect(400)
+       
+    })
 
-    assert.strictEqual(response.body.length, manyBlogs.length)
 })
+
+describe('DELETE works', () => {
+    test('Deleting first blog', async () => {
+        const id = manyBlogs[0]._id
+        await api.delete(`/api/blogs/${id}`).expect(204)
+    })
+})
+
+
+// I didnt got this to work:((
+describe('PUT works', () => {
+
+    test('Changing first blogs likes works', async () => {
+        const id = manyBlogs[0]._id
+        console.log(id)
+        const changedLikes = {
+            title: manyBlogs[0].title,
+            author: manyBlogs[0].author,
+            url: manyBlogs[0].url,
+            likes: 1,
+        }
+
+        const response = await api.put(`/api/blogs/${id}`).send(changedLikes)
+        console.log(response.body)
+
+    })
+
+})
+
 
 
 after(async () => {
